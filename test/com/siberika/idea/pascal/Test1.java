8871868dcbde0e3a5b96ca34eff072e413d7a63f
George Bakhtadze <argb32@gmail.com>
2013-03-21 01:03:24 +0700

    Conversion to Maven project and test directory

---
+++ test/com/siberika/idea/pascal/Test1.java
@@ -0,0 +1,13 @@
+package com.siberika.idea.pascal;
+
+import org.junit.Test;
+
+/**
+ * Author: George Bakhtadze
+ * Date: 20/03/2013
+ */
+public class Test1 {
+    @Test
+    public void test() throws Exception {
+    }
+}
