9e84ab3e4a00ebeb18dec7e79ff4cb606f895647
George Bakhtadze <argb32@gmail.com>
2013-11-04 18:30:31 +0700

    Editor highlighter classes

---
+++ plugin/src/lang/lexer/PascalEditorHighlighterProvider.java
@@ -0,0 +1,30 @@
+package com.siberika.idea.pascal.lang.lexer;
+
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.highlighter.EditorHighlighter;
+import com.intellij.openapi.fileTypes.EditorHighlighterProvider;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.siberika.idea.pascal.PascalFileType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Author: George Bakhtadze
+ * Date: 28/08/2013
+ */
+public class PascalEditorHighlighterProvider implements EditorHighlighterProvider {
+    @Override
+    public EditorHighlighter getEditorHighlighter(@Nullable Project project, @NotNull FileType fileType, @Nullable VirtualFile virtualFile, @NotNull EditorColorsScheme colors) {
+        if (fileType == PascalFileType.INSTANCE) {
+            SyntaxHighlighter syntaxHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(fileType, project, virtualFile);
+            if (syntaxHighlighter != null) {
+                return new PascalLexerEditorHighlighter(syntaxHighlighter, colors, project, virtualFile);
+            }
+        }
+        return null;
+    }
+}
---
+++ plugin/src/lang/lexer/PascalLexerEditorHighlighter.java
@@ -0,0 +1,52 @@
+package com.siberika.idea.pascal.lang.lexer;
+
+import com.intellij.lexer.FlexLexer;
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.event.DocumentEvent;
+import com.intellij.openapi.editor.ex.util.LexerEditorHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Author: George Bakhtadze
+ * Date: 28/08/2013
+ */
+public class PascalLexerEditorHighlighter extends LexerEditorHighlighter {
+
+    public PascalLexerEditorHighlighter(@NotNull SyntaxHighlighter highlighter, @NotNull EditorColorsScheme scheme, @Nullable Project project, @Nullable VirtualFile virtualFile) {
+        super(highlighter, scheme);
+        PascalFlexLexerImpl pascalFlexLexer = getPascalFlexLexerImpl();
+        if (pascalFlexLexer != null) {
+            pascalFlexLexer.project = project;
+            pascalFlexLexer.virtualFile = virtualFile;
+        }
+    }
+
+    @Override
+    public synchronized void documentChanged(DocumentEvent e) {
+        super.documentChanged(e);
+
+        if (getDocument() != null) {
+            // reset highlighting lexer cache
+            //noinspection ConstantConditions
+            setText(getDocument().getCharsSequence());
+            // clear conditional defines
+            PascalFlexLexerImpl pascalFlexLexer = getPascalFlexLexerImpl();
+        }
+    }
+
+    public PascalFlexLexerImpl getPascalFlexLexerImpl() {
+        Lexer lexer = getLexer();
+        if (lexer instanceof PascalLexer) {
+            FlexLexer flexLexer = ((PascalLexer) lexer).getFlexLexer();
+            if (flexLexer instanceof PascalFlexLexerImpl) {
+                return (PascalFlexLexerImpl) flexLexer;
+            }
+        }
+        return null;
+    }
+}
