c333693fa47e5236a09cb56c0aa33d8e24dabc10
George Bakhtadze <argb32@gmail.com>
2015-01-27 16:03:35 +0300

    [+] Restricted completion

--- plugin/src/editor/completion/PascalCompletionConfidence.java
+++ plugin/src/editor/completion/PascalCompletionConfidence.java
@@ -1,8 +1,11 @@
 package com.siberika.idea.pascal.editor.completion;
 
 import com.intellij.codeInsight.completion.CompletionConfidence;
-import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.openapi.util.NullUtils;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.util.ThreeState;
+import com.siberika.idea.pascal.lang.psi.PasTypes;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -12,8 +15,16 @@ import org.jetbrains.annotations.NotNull;
 public class PascalCompletionConfidence extends CompletionConfidence {
     @NotNull
     @Override
-    public ThreeState shouldFocusLookup(@NotNull CompletionParameters parameters) {
-        return ThreeState.YES;
-    }
+    public ThreeState shouldSkipAutopopup(@NotNull PsiElement contextElement, @NotNull PsiFile psiFile, int offset) {
+        if (NullUtils.notNull(contextElement, contextElement.getPrevSibling(), contextElement.getPrevSibling().getNode())) {
 
+            /*System.out.println("===*** shouldSkipAutopopup: " + contextElement + ", sib: " + contextElement.getPrevSibling()
+                    + ", par: " + contextElement.getParent());*/
+
+            if (contextElement.getPrevSibling().getNode().getElementType() != PasTypes.NAME) {
+                return ThreeState.YES;
+            }
+        }
+        return super.shouldSkipAutopopup(contextElement, psiFile, offset);
+    }
 }
