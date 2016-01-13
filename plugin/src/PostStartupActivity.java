83dbdb96dbc2b1fcb295251876e48ecd5bc15bef
George Bakhtadze <argb32@gmail.com>
2016-01-13 18:39:25 +0300

    [*] project service replaced by startup activity

--- plugin/src/PostStartupActivity.java
+++ plugin/src/PostStartupActivity.java
@@ -4,41 +4,19 @@ import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
-import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Author: George Bakhtadze
- * Date: 03/01/2016
+ * Date: 13/01/2016
  */
-public class PascalProjectService implements ProjectComponent {
-
-    public PascalProjectService() {
-    }
-
+public class PostStartupActivity implements StartupActivity {
     @Override
-    public void projectOpened() {
+    public void runActivity(@NotNull Project project) {
         Notifications.Bus.notify(new Notification(PascalAppService.PASCAL_NOTIFICATION_GROUP, PascalBundle.message("app.welcome.title"),
                 PascalBundle.message("app.welcome.text"), NotificationType.INFORMATION,
                 new NotificationListener.UrlOpeningListener(true)));
-    }
-
-    @Override
-    public void projectClosed() {
-
-    }
-
-    @Override
-    public void initComponent() {
-    }
-
-    @Override
-    public void disposeComponent() {
-    }
-
-    @NotNull
-    @Override
-    public String getComponentName() {
-        return PascalBundle.message("app.title");
     }
 }
