f48c08048aa2cc5b4de58b2c17ab33033e0385e8
George Bakhtadze <argb32@gmail.com>
2016-01-20 15:58:56 +0300

    [*] show welcome message only when Pascal modules are in project or for empty projects

--- plugin/src/PostStartupActivity.java
+++ plugin/src/PostStartupActivity.java
@@ -4,8 +4,11 @@ import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import com.siberika.idea.pascal.module.PascalModuleType;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -15,8 +18,15 @@ import org.jetbrains.annotations.NotNull;
 public class PostStartupActivity implements StartupActivity {
     @Override
     public void runActivity(@NotNull Project project) {
-        Notifications.Bus.notify(new Notification(PascalAppService.PASCAL_NOTIFICATION_GROUP, PascalBundle.message("app.welcome.title"),
-                PascalBundle.message("app.welcome.text"), NotificationType.INFORMATION,
-                new NotificationListener.UrlOpeningListener(true)));
+        Module[] modules = ModuleManager.getInstance(project).getModules();
+        boolean hasPascalModules = false;
+        for (Module module : modules) {
+            hasPascalModules |= PascalModuleType.isPascalModule(module);
+        }
+        if (hasPascalModules || (modules.length == 0)) {
+            Notifications.Bus.notify(new Notification(PascalAppService.PASCAL_NOTIFICATION_GROUP, PascalBundle.message("app.welcome.title"),
+                    PascalBundle.message("app.welcome.text"), NotificationType.INFORMATION,
+                    new NotificationListener.UrlOpeningListener(true)));
+        }
     }
 }
