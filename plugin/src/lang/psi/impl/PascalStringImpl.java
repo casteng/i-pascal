29992c1c6c1722ee7bc6edf88992e149c3db0b5f
George Bakhtadze <argb32@gmail.com>
2016-01-22 01:42:09 +0300

    [+] ability to inject other languages in Pascal string literals

---
+++ plugin/src/lang/psi/impl/PascalStringImpl.java
@@ -0,0 +1,78 @@
+package com.siberika.idea.pascal.lang.psi.impl;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.ProperTextRange;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.LiteralTextEscaper;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import com.intellij.psi.PsiLiteral;
+import com.intellij.psi.impl.source.tree.LeafElement;
+import com.siberika.idea.pascal.lang.psi.PascalPsiElement;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Author: George Bakhtadze
+ * Date: 21/01/2016
+ */
+public abstract class PascalStringImpl extends PascalPsiElementImpl implements PsiLanguageInjectionHost, PascalPsiElement, PsiLiteral {
+
+    public PascalStringImpl(ASTNode node) {
+        super(node);
+    }
+
+    @Override
+    public boolean isValidHost() {
+        return true;
+    }
+
+    @Override
+    public PascalStringImpl updateText(@NotNull String text) {
+        ASTNode valueNode = getNode().getFirstChildNode();
+        assert valueNode instanceof LeafElement;
+        ((LeafElement)valueNode).replaceWithText(text);
+        return this;
+    }
+
+    @Nullable
+    @Override
+    public String getValue() {
+        return getText().length() > 2 ? getText().substring(1, getText().length() - 1) : "";
+    }
+
+    @NotNull
+    @Override
+    public LiteralTextEscaper<? extends PsiLanguageInjectionHost> createLiteralTextEscaper() {
+        return new LiteralTextEscaper<PsiLanguageInjectionHost>(this) {
+
+            private String lastDecoded;
+
+            @Override
+            public boolean decode(@NotNull TextRange rangeInsideHost, @NotNull StringBuilder outChars) {
+                ProperTextRange.assertProperRange(rangeInsideHost);
+                String value = myHost.getText().substring(rangeInsideHost.getStartOffset(), rangeInsideHost.getEndOffset());
+                lastDecoded = value.replace("''", "'");
+                outChars.append(lastDecoded);
+                return false;
+            }
+
+            @Override
+            public int getOffsetInHost(int offsetInDecoded, @NotNull TextRange rangeInsideHost) {
+                ProperTextRange.assertProperRange(rangeInsideHost);
+                int offset = rangeInsideHost.getStartOffset() + offsetInDecoded;
+                int escapedCount = StringUtils.countMatches(lastDecoded.substring(0, offsetInDecoded), "'");
+                offset += escapedCount;
+                if (offset < rangeInsideHost.getStartOffset()) offset = rangeInsideHost.getStartOffset();
+                if (offset > rangeInsideHost.getEndOffset()) offset = rangeInsideHost.getEndOffset();
+                return offset;
+            }
+
+            @Override
+            public boolean isOneLine() {
+                return true;
+            }
+        };
+    }
+
+}
---
+++ plugin/src/lang/references/impl/manipulators/PascalStringManipulator.java
@@ -0,0 +1,30 @@
+package com.siberika.idea.pascal.lang.references.impl.manipulators;
+
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.AbstractElementManipulator;
+import com.intellij.util.IncorrectOperationException;
+import com.siberika.idea.pascal.lang.psi.impl.PascalStringImpl;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Author: George Bakhtadze
+ * Date: 21/01/2016
+ */
+public class PascalStringManipulator extends AbstractElementManipulator<PascalStringImpl> {
+    @Override
+    public PascalStringImpl handleContentChange(@NotNull PascalStringImpl psi, @NotNull TextRange range, String newContent) throws IncorrectOperationException {
+        final String oldText = psi.getText();
+        final String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
+        return psi.updateText(newText);
+    }
+
+    @NotNull
+    @Override
+    public TextRange getRangeInElement(@NotNull final PascalStringImpl element) {
+        return getStringTokenRange(element);
+    }
+
+    public static TextRange getStringTokenRange(final PascalStringImpl element) {
+        return TextRange.from(1, element.getTextLength()-2);
+    }
+}
