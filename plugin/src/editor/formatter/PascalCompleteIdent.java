7d3ba08ce14b41a97e6cd4c08ea4eea7e3d1643d
George Bakhtadze <argb32@gmail.com>
2016-08-30 01:35:17 +0300

    [+] complete variable declaration

--- plugin/src/editor/formatter/PascalCompleteIdent.java
+++ plugin/src/editor/formatter/PascalCompleteIdent.java
@@ -2,6 +2,8 @@ package com.siberika.idea.pascal.editor.formatter;
 
 import com.google.common.collect.Iterables;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiErrorElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.siberika.idea.pascal.lang.psi.PasNamedIdent;
 import com.siberika.idea.pascal.lang.psi.PasTypes;
 import com.siberika.idea.pascal.lang.psi.PascalVariableDeclaration;
@@ -17,15 +19,17 @@ public class PascalCompleteIdent {
 
     public static void completeIdent(Editor editor, PascalVariableDeclaration el) {
         /**
-         *      => _:;
          * name => name: _;
          */
-        int colonPos = getChildEndOffset(PasTypes.COLON, el);
-        String colonStr = colonPos >= 0 ? "" : ": ";
-        String rparenStr = "";
-        PasNamedIdent last = Iterables.getLast(el.getNamedIdentList(), null);
-        if (last != null) {
-            DocUtil.adjustDocument(editor, colonPos >= 0 ? colonPos + 1 : last.getTextRange().getEndOffset(), colonStr + DocUtil.PLACEHOLDER_CARET + rparenStr);
+        PsiErrorElement error = PsiTreeUtil.findChildOfType(el, PsiErrorElement.class);
+        if ((el.getTypeDecl() == null) || (error != null)) {
+            int colonPos = getChildEndOffset(PasTypes.COLON, el);
+            String colonStr = colonPos >= 0 ? "" : ": ";
+            String rparenStr = ";";
+            PasNamedIdent last = Iterables.getLast(el.getNamedIdentList(), null);
+            if (last != null) {
+                DocUtil.adjustDocument(editor, colonPos >= 0 ? colonPos + 1 : last.getTextRange().getEndOffset(), colonStr + DocUtil.PLACEHOLDER_CARET + rparenStr);
+            }
         }
     }
 
