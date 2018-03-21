00e3bba176cae96d9a1d538ad74e68ce9c46435b
George Bakhtadze <argb32@gmail.com>
2018-03-22 02:12:09 +0300

    [+] reestablish search for a symbol and search for a class by name features using stub indices

--- plugin/src/lang/references/PascalChooseByNameContributor.java
+++ plugin/src/lang/references/PascalChooseByNameContributor.java
@@ -3,11 +3,17 @@ package com.siberika.idea.pascal.lang.references;
 import com.intellij.navigation.ChooseByNameContributor;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.project.Project;
-import com.siberika.idea.pascal.lang.parser.PascalParserUtil;
+import com.intellij.psi.stubs.StubIndex;
+import com.intellij.util.Processor;
+import com.intellij.util.containers.SmartHashSet;
 import com.siberika.idea.pascal.lang.psi.PascalNamedElement;
+import com.siberika.idea.pascal.lang.stub.PascalSymbolIndex;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Date: 3/14/13
@@ -17,22 +23,40 @@ public class PascalChooseByNameContributor implements ChooseByNameContributor {
     @NotNull
     @Override
     public String[] getNames(Project project, boolean includeNonProjectItems) {
-        return new String[0];
-        /*Collection<PascalNamedElement> properties = PascalParserUtil.findSymbols(project, "");
-        List<String> names = new ArrayList<String>(properties.size());
-        for (PascalNamedElement property : properties) {
-            if (property.getName().length() > 0) {
-                names.add(property.getName());
+        List<String> names = new ArrayList<String>();
+        StubIndex.getInstance().processAllKeys(PascalSymbolIndex.KEY, new Processor<String>() {
+            @Override
+            public boolean process(String key) {
+                names.add(key);
+                return true;
             }
-        }
-        return names.toArray(new String[names.size()]);*/
+        }, PascalClassByNameContributor.getScope(project, includeNonProjectItems), null);
+        return names.toArray(new String[names.size()]);
     }
 
     @NotNull
     @Override
     public NavigationItem[] getItemsByName(String name, String pattern, Project project, boolean includeNonProjectItems) {
-        // todo include non project items
-        Collection<PascalNamedElement> items = PascalParserUtil.findSymbols(project, pattern);
+        Collection<PascalNamedElement> items = new SmartHashSet<>();
+        final Pattern p = Pattern.compile("\\w*" + pattern + "\\w*");
+
+        StubIndex.getInstance().processAllKeys(PascalSymbolIndex.KEY, new Processor<String>() {
+            @Override
+            public boolean process(final String key) {
+                if (p.matcher(key).matches()) {
+                    StubIndex.getInstance().processElements(PascalSymbolIndex.KEY, key, project, PascalClassByNameContributor.getScope(project, includeNonProjectItems),
+                            PascalNamedElement.class, new Processor<PascalNamedElement>() {
+                                @Override
+                                public boolean process(PascalNamedElement namedElement) {
+                                    items.add(namedElement);
+                                    return true;
+                                }
+                            });
+                }
+                return true;
+            }
+        }, PascalClassByNameContributor.getScope(project, includeNonProjectItems), null);
+
         return items.toArray(new NavigationItem[items.size()]);
     }
 }
--- plugin/src/lang/references/PascalClassByNameContributor.java
+++ plugin/src/lang/references/PascalClassByNameContributor.java
@@ -3,11 +3,19 @@ package com.siberika.idea.pascal.lang.references;
 import com.intellij.navigation.ChooseByNameContributor;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.project.Project;
-import com.siberika.idea.pascal.lang.parser.PascalParserUtil;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.stubs.StubIndex;
+import com.intellij.util.Processor;
+import com.intellij.util.containers.SmartHashSet;
 import com.siberika.idea.pascal.lang.psi.PascalNamedElement;
+import com.siberika.idea.pascal.lang.psi.PascalStructType;
+import com.siberika.idea.pascal.lang.stub.PascalStructIndex;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Date: 3/14/13
@@ -17,22 +25,49 @@ public class PascalClassByNameContributor implements ChooseByNameContributor {
     @NotNull
     @Override
     public String[] getNames(Project project, boolean includeNonProjectItems) {
-        return new String[0];
-        /*Collection<PascalNamedElement> properties = PascalParserUtil.findClasses(project, "");
-        List<String> names = new ArrayList<String>(properties.size());
-        for (PascalNamedElement property : properties) {
-            if (property.getName().length() > 0) {
-                names.add(property.getName());
+        List<String> names = new ArrayList<String>();
+        StubIndex.getInstance().processAllKeys(PascalStructIndex.KEY, new Processor<String>() {
+            @Override
+            public boolean process(String key) {
+                names.add(keyToName(key));
+                return true;
             }
-        }
-        return names.toArray(new String[names.size()]);*/
+        }, getScope(project, includeNonProjectItems), null);
+        return names.toArray(new String[names.size()]);
+    }
+
+    static GlobalSearchScope getScope(Project project, boolean includeNonProjectItems) {
+        return includeNonProjectItems ? GlobalSearchScope.allScope(project) : GlobalSearchScope.projectScope(project);
+    }
+
+    static String keyToName(String key) {
+        int ind = key.indexOf('.');
+        return key.substring(ind + 1).replaceAll("#", "");
     }
 
     @NotNull
     @Override
     public NavigationItem[] getItemsByName(String name, String pattern, Project project, boolean includeNonProjectItems) {
-        // todo include non project items
-        Collection<PascalNamedElement> items = PascalParserUtil.findClasses(project, pattern);
+        Collection<PascalNamedElement> items = new SmartHashSet<>();
+        final Pattern p = Pattern.compile("\\w*" + pattern + "\\w*");
+
+        StubIndex.getInstance().processAllKeys(PascalStructIndex.KEY, new Processor<String>() {
+            @Override
+            public boolean process(final String key) {
+                if (p.matcher(keyToName(key)).matches()) {
+                    StubIndex.getInstance().processElements(PascalStructIndex.KEY, key, project, getScope(project, includeNonProjectItems),
+                            PascalStructType.class, new Processor<PascalStructType>() {
+                                @Override
+                                public boolean process(PascalStructType structType) {
+                                    items.add(structType);
+                                    return true;
+                                }
+                            });
+                }
+                return true;
+            }
+        }, getScope(project, includeNonProjectItems), null);
+
         return items.toArray(new NavigationItem[items.size()]);
     }
 
