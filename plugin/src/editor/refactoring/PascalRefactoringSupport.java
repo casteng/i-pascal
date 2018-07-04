87b0a4a9f6f1024f1ac97d4bfcff30ae5b130166
George Bakhtadze <argb32@gmail.com>
2018-07-05 01:28:48 +0300

    [-] fix rename of method to different case (Issue #29), synchronize implementation/interface part when rename a method parameter

--- plugin/src/editor/refactoring/PascalRefactoringSupport.java
+++ plugin/src/editor/refactoring/PascalRefactoringSupport.java
@@ -2,17 +2,26 @@ package com.siberika.idea.pascal.editor.refactoring;
 
 import com.intellij.lang.refactoring.RefactoringSupportProvider;
 import com.intellij.psi.PsiElement;
+import com.siberika.idea.pascal.lang.psi.PasFormalParameter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class PascalRefactoringSupport extends RefactoringSupportProvider {
     @Override
     public boolean isInplaceRenameAvailable(@NotNull PsiElement element, PsiElement context) {
+        PsiElement parent = element.getParent();
+        if (parent instanceof PasFormalParameter) {
+            return false;
+        }
         return true;
     }
 
     @Override
     public boolean isMemberInplaceRenameAvailable(@NotNull PsiElement element, @Nullable PsiElement context) {
+        PsiElement parent = element.getParent();
+        if (parent instanceof PasFormalParameter) {
+            return false;
+        }
         return true;
     }
 }
