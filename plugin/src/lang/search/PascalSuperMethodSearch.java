28e70d5f48fee480daaa0777f855b6d2655cbbc5
George Bakhtadze <argb32@gmail.com>
2019-09-01 20:39:02 +0300

    [-] fix invalid class cast (Issue #96)

--- plugin/src/lang/search/PascalSuperMethodSearch.java
+++ plugin/src/lang/search/PascalSuperMethodSearch.java
@@ -5,11 +5,13 @@ import com.intellij.util.Processor;
 import com.siberika.idea.pascal.lang.psi.PasEntityScope;
 import org.jetbrains.annotations.NotNull;
 
-public class PascalSuperMethodSearch extends QueryExecutorBase<PasEntityScope, GotoSuper.OptionsRoutine> {
+public class PascalSuperMethodSearch extends QueryExecutorBase<PasEntityScope, Object> {
 
     @Override
-    public void processQuery(@NotNull GotoSuper.OptionsRoutine queryParameters, @NotNull Processor<? super PasEntityScope> consumer) {
-        GotoSuper.searchForRoutine(queryParameters.getElement()).forEach(consumer);
+    public void processQuery(@NotNull Object queryParameters, @NotNull Processor<? super PasEntityScope> consumer) {
+        if (queryParameters instanceof GotoSuper.OptionsRoutine) {            // Workaround for issue https://bitbucket.org/argb32/i-pascal/issues/96/plugin-throws-casscastexception
+            GotoSuper.searchForRoutine(((GotoSuper.OptionsRoutine) queryParameters).getElement()).forEach(consumer);
+        }
     }
 
 }
